266d7a916007b69a0a78da4435d722ccd93970cc
loloups <loic.leger@univ-tlse3.fr>
2015-02-09 15:10:34 +0100

    Premier Commit

---
+++ src/main/java/dcll/lleg/App.java
@@ -0,0 +1,13 @@
+package dcll.lleg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
